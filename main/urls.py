751560e9e614400cd3af88cf3e50d4fb7d0356e4
isaacboentoro <isaac.jesse@ui.ac.id>
2024-10-07 13:19:03 +0700

    No AJAX modal but asynchronous product loading using JSON works.

--- main/urls.py
+++ main/urls.py
@@ -11,11 +11,12 @@ from main.views import (
     logout_user,
     edit_product,
     delete_product,
+    create_product_ajax,
 )
 
 app_name = 'main'
 urlpatterns = [
-        path('', show_main, name = 'show_main'),
+    path('', show_main, name = 'show_main'),
         path('create-product', create_product, name='create_product'),
         path('json/', show_json, name='show_json'),
         path('xml/', show_xml, name='show_xml' ),
@@ -25,5 +26,6 @@ urlpatterns = [
         path('login/', login_user, name='login'),
         path('logout/', logout_user, name='logout'),
         path('edit-product/<uuid:id>', edit_product, name='edit_product'),
-        path('delete/<uuid:id>', delete_product, name='delete_product')
+        path('delete/<uuid:id>', delete_product, name='delete_product'),
+        path('create-product-ajax', create_product_ajax, name='create_product_ajax'),
 ]
